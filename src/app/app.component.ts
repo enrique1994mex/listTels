6a76564bb3e6e8a63d1436c33639e3f72f56633b
Enrique Sanchez Sandoval <enrique.antonio1029@gmail.com>
2023-06-26 03:34:16 -0600

    add delete and update Tel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,56 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  tels: string[] = ['9991009980', '9993265580', '9991526620'];
+  newTel: string = '';
+  deleteTel: string = '';
+  updateTel: string = '';
+  newUpdateTel: string = '';
+
+  constructor(private modalService: NgbModal) {}
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
+
+  onAdd() {
+    if (this.tels.length < 3) {
+      this.tels.push(this.newTel);
+    }
+    this.newTel = '';
+    this.modalService.dismissAll();
+  }
+
+  onAddDelete(tel: string) {
+    this.deleteTel = tel;
+  }
+
+  onDelete() {
+    this.tels = this.tels.filter((tel) => tel !== this.deleteTel);
+    this.modalService.dismissAll();
+    this.deleteTel = '';
+  }
+
+  onAddUpdate(tel: string) {
+    this.updateTel = tel;
+    this.newUpdateTel = tel;
+  }
+
+  onUpdate() {
+    this.tels = this.tels.map((tel) => {
+      if (tel === this.updateTel) {
+        return this.newUpdateTel;
+      } else {
+        return tel;
+      }
+    });
+    this.modalService.dismissAll();
+    this.updateTel = '';
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgbModule, AppRoutingModule],
+  imports: [BrowserModule, NgbModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
